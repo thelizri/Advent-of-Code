99b3b633fe52eb689a2de35eb33bd8cdf7626f09
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-02-26 16:22:37 +0100

    Day 3 Part 2 Done

--- 2021/day3.py
+++ 2021/day3.py
@@ -1,3 +1,4 @@
+#Part 1
 file = open("day3.txt", "r")
 mylist = []
 length = 1
@@ -13,6 +14,7 @@ for row in file:
 	for i in range(len(mylist)):
 		if(row[i] == "1"):
 			mylist[i] += 1
+file.close()
 gamma = ""
 epsilon = ""
 for n in mylist:
@@ -26,3 +28,64 @@ for n in mylist:
 result = int(epsilon, 2)*int(gamma, 2)
 print("Part 1: "+str(result))
 
+#Part 2
+def getRows():
+	file = open("day3.txt", "r")
+	result = []
+	for row in file:
+		result.append(row.replace("\n", ""))
+	file.close()
+	return result
+
+def getMostCommon(file, pos, default):
+	num1 = 0
+	num0 = 0
+	for row in file:
+		if row[pos] == "1":
+			num1 += 1
+		else:
+			num0 += 1
+	if num1 > num0:
+		return "1"
+	elif num1 == num0:
+		return default
+	else:
+		return "0"
+
+def getLeastCommon(file, pos, default):
+	num1 = 0
+	num0 = 0
+	for row in file:
+		if row[pos] == "1":
+			num1 += 1
+		else:
+			num0 += 1
+	if num1 < num0:
+		return "1"
+	elif num1 == num0:
+		return default
+	else:
+		return "0"
+
+
+#mylist = list(filter(lambda x: x[0]==common, mylist))
+mylist = getRows()
+length = len(mylist[0])
+pos = 0
+while len(mylist) > 1:
+	common = getMostCommon(mylist, pos, "1")
+	mylist = list(filter(lambda x: x[pos]==common, mylist))
+	pos += 1
+oxygen = int(mylist[0], 2)
+
+mylist = getRows()
+length = len(mylist[0])
+pos = 0
+while len(mylist) > 1:
+	common = getLeastCommon(mylist, pos, "0")
+	mylist = list(filter(lambda x: x[pos]==common, mylist))
+	pos += 1
+co2rating = int(mylist[0], 2)
+
+result = oxygen*co2rating
+print("Part 2: "+str(result))
