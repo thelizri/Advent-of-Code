a0d667df69474dd22fcb1778a6abaa28d3925859
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-02-26 02:15:32 +0100

    Day 1 of 2021 Finished

---
+++ 2021/day1.py
@@ -0,0 +1,26 @@
+#Part 1
+file = open("day1.txt", "r")
+previous = int(file.readline())
+counter = 0
+
+for row in file:
+	if previous<int(row):
+		counter += 1
+	previous = int(row)
+
+print("Part 1: " + str(counter))
+
+#Part 2
+file = open("day1.txt", "r")
+a = int(file.readline())
+b = int(file.readline())
+c = int(file.readline())
+counter = 0
+for row in file:
+	prev = a + b + c
+	current = b + c + int(row)
+	if prev < current:
+		counter += 1
+	a, b, c = b, c, int(row)
+
+print("Part 2: " + str(counter))
