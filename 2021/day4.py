6eb8400cc88c495f5e7f1c6ad972e7d8fe989dbe
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-03-01 00:20:03 +0100

    Update

--- 2021/day4.py
+++ 2021/day4.py
@@ -2,26 +2,7 @@ prime = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67,
 products = [9059606, 44676511, 2310, 2800733, 95041567, 907383479, 4132280413, 3118414, 8795307, 19720385, 41281849, 103256791]
 #Diagonal top-left to bottom-right, Diagonal bottom-left to top-right, row 0-4, col 0-4
 
-product = 1
-for i in range(0, 25, 6):
-	product *= prime[i]
-products.append(product)
-
-product = 1
-for i in range(4, 21, 4):
-	product *= prime[i]
-products.append(product)
-
-for row in range(0, 5):
-	product = 1
-	for i in range(0, 5):
-		product *= prime[i+row*5]
-	products.append(product)
-
-for col in range(0, 5):
-	product = 1
-	for i in range(0, 5):
-		product *= prime[col + i*5]
-	products.append(product)
-
-print(products)
+file = open("day4.txt")
+bingo_numbers = file.readline().strip().split(",")
+bingo_numbers = list(map(lambda x: int(x), bingo_numbers))
+print(bingo_numbers)
