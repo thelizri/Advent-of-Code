361f812ac2a1ee6117d720fb276d155c071d593d
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-03-03 22:10:30 +0100

    Refactor

--- 2021/day4.py
+++ 2021/day4.py
@@ -5,31 +5,11 @@ import re
 # Step 4: If they do. Take the index, get the prime at that index, and multiply the state with it.
 # Step 5: Check if any of the boards has bingo. If they do, calculate the score.
 
+bingo_boards = []
+bingo_states = []
 prime = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97] #First 25 prime numbers
 products = [9059606, 44676511, 2310, 2800733, 95041567, 907383479, 4132280413, 3118414, 8795307, 19720385, 41281849, 103256791] # We can use these numbers to check if a bingo board has won
 #Diagonal top-left to bottom-right, Diagonal bottom-left to top-right, row 0-4, col 0-4
-
-file = open("day4.txt")
-bingo_numbers = file.readline().strip().split(",")
-bingo_numbers = list(map(lambda x: int(x), bingo_numbers))
-
-rows = list(map(lambda x: x.strip(), file.readlines()))
-rows = list(filter(lambda x: x, rows))
-
-i = 0
-bingo_boards = []
-while i < len(rows)/5:
-	board = []
-	for j in range(0,5):
-		row = rows[j+i*5]
-		nums = list(map(lambda x: int(x), re.findall("\d+", row)))
-		for num in nums: board.append(num)
-	bingo_boards.append(board)
-	i += 1
-
-bingo_states = []
-for i in bingo_boards:
-	bingo_states.append(1)
 
 def checkIfBoardHasWon(board_state):
 	for i in products:
@@ -48,6 +28,28 @@ def getWinningBoard():
 		if checkIfBoardHasWon(bingo_states[i]):
 			return i
 	return -1
+
+file = open("day4.txt")
+bingo_numbers = file.readline().strip().split(",")
+bingo_numbers = list(map(lambda x: int(x), bingo_numbers))
+
+rows = list(map(lambda x: x.strip(), file.readlines()))
+rows = list(filter(lambda x: x, rows))
+
+#Init bingo boards
+i = 0
+while i < len(rows)/5:
+	board = []
+	for j in range(0,5):
+		row = rows[j+i*5]
+		nums = list(map(lambda x: int(x), re.findall("\d+", row)))
+		for num in nums: board.append(num)
+	bingo_boards.append(board)
+	i += 1
+
+#Init states
+for i in bingo_boards:
+	bingo_states.append(1)
 
 for bingo in bingo_numbers:
 	addBingoNumberToAllBoards(bingo)
