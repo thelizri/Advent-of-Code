93db166a757735b715163e83f57928a0277c0521
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-03-07 23:32:14 +0100

    Day 7 Part 2 Done

--- 2021/day7.py
+++ 2021/day7.py
@@ -6,6 +6,14 @@ def calcMoves(li, pos):
 		totalMoves += abs(x-pos)
 	return totalMoves
 
+#1+2+3+...+n=n(n+1)/2 
+def calcFuelPart2(li, pos):
+	totalMoves = 0
+	for x in li:
+		n = abs(x-pos)
+		totalMoves += n*(n+1)/2
+	return int(totalMoves)
+
 mylist = []
 file = open("day7.txt")
 for row in file:
@@ -13,11 +21,14 @@ for row in file:
 	numbers = map(lambda x: int(x), numbers)
 	mylist.extend(numbers)
 
-result = []
+part1 = []
+part2 = []
 small = min(mylist)
 large = max(mylist)+1
 for num in range(small, large):
-	result.append(calcMoves(mylist, num))
+	part1.append(calcMoves(mylist, num))
+	part2.append(calcFuelPart2(mylist, num))
 
-print(min(result))
 
+print("Part 1: "+str(min(part1)))
+print("Part 2: "+str(min(part2)))
