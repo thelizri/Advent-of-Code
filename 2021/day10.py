cd28ed37c3a07b2f98d83f3a8e2df4dac0974e53
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-03-13 00:44:32 +0100

    Part 2, Day 10, 2021

--- 2021/day10.py
+++ 2021/day10.py
@@ -29,14 +29,55 @@ score = {")": 3, "]": 57, "}": 1197, ">": 25137}
 
 points = 0
 stack = []
+corrupted_lines = []
+index = -1
 for row in text_list:
+	index += 1
 	for char in row:
 		if isOpening(char): stack.append(char)
 		else:
 			res = isMatch(stack.pop(), char)
 			if not res:
 				points += score.get(char)
+				corrupted_lines.append(index)
 				break;
 	stack.clear()
 
 print("Part 1: "+str(points))
+
+#Part 2
+#remove corrupted_lines
+for i in range(0, len(corrupted_lines)):
+	index = corrupted_lines[i] - i
+	text_list.pop(index)
+
+# ): 1 point.
+# ]: 2 points.
+# }: 3 points.
+# >: 4 points.
+score = {")": 1, "]": 2, "}": 3, ">": 4}
+closer = {"(":")", "[":"]", "{":"}", "<":">"}
+points = 0
+
+def updateScore(closingChar):
+	global points
+	points = points*5 + score.get(closingChar)
+
+stack = []
+total_points = []
+for row in text_list:
+	for char in row:
+		if isOpening(char): stack.append(char)
+		else: stack.pop()
+	stack.reverse()
+	for char in stack:
+		updateScore(closer.get(char))
+	stack.clear()
+	total_points.append(points)
+	points = 0
+
+#Get middle score
+total_points.sort()
+index = len(total_points)//2
+answer = total_points[index]
+print("Part 2: "+str(answer))
