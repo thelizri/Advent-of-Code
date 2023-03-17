53be05e0723d087bf19b695600a463ed0bd6b3b4
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-03-17 23:55:10 +0100

    Reading input

--- 2021/day12.py
+++ 2021/day12.py
@@ -1,11 +1,21 @@
-def decrement(map):
-    map1 = dict()
-    for k, v in map.items():
-        if k == 0:
-            map1.update({6 : v})
-        else:
-            map1.update({k-1 : v})
-    return map1 
-print({2: 45, 3: 49, 1: 107, 4: 49, 0: 50})
-map1 = decrement({2: 45, 3: 49, 1: 107, 4: 49, 0: 50})
-print(map1)
+file = open("day12.txt", "r")
+file.seek(0)
+
+def addNode(graph, key, node):
+    li = graph.get(key)
+    if node not in li:
+        li.append(node)
+
+graph = dict()
+for row in file:
+    [a, b] = row.strip().split("-")
+    if a in graph:
+        addNode(graph, a, b)
+    else:
+        graph.update({a: [b]})
+    if b in graph:
+        addNode(graph, b, a)
+    else:
+        graph.update({b: [a]})
+
+print(graph)
