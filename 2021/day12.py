a00f25a71afb402234dbe2969b5ecf18f8453987
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-03-18 00:31:18 +0100

    update

--- 2021/day12.py
+++ 2021/day12.py
@@ -1,3 +1,6 @@
+#Your goal is to find the number of distinct paths that start at start, end at end, 
+#and don't visit small caves more than once.
+
 file = open("day12.txt", "r")
 file.seek(0)
 
@@ -18,4 +21,21 @@ for row in file:
     else:
         graph.update({b: [a]})
 
-print(graph)
+nodes = list(graph.keys())
+nodes.remove("start")
+
+def dfs_search(node, graph, unvisited):
+    if node == "end": return 1
+    neigh = graph.get(node).copy()
+    if "start" in neigh: neigh.remove("start")
+    total = 0
+    for n in neigh:
+        if n in unvisited:
+            copy = unvisited.copy()
+            if n.islower(): copy.remove(n)
+            total += dfs_search(n, graph, copy)
+    return total
+
+print(dfs_search("start", graph, nodes))
+
+# isupper(), islower()
