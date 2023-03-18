5210f1815c45e8487ba3f6cb1022a4d92f959607
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-03-18 01:37:31 +0100

    Update

--- 2021/day12.py
+++ 2021/day12.py
@@ -36,6 +36,28 @@ def dfs_search(node, graph, unvisited):
             total += dfs_search(n, graph, copy)
     return total
 
-print(dfs_search("start", graph, nodes))
+part1 = dfs_search("start", graph, nodes)
+print("Part 1: "+str(part1))
 
-# isupper(), islower()
+#Part 2 Gives wrong result
+def dfs_search2(node, graph, unvisited, visited):
+    if node == "end": return 1
+    if node != "start" and node.islower(): visited = True
+
+    neigh = graph.get(node).copy()
+    if "start" in neigh: neigh.remove("start")
+
+    total = 0
+    for n in neigh:
+        if n in unvisited:
+            copy_unvisited = unvisited.copy()
+            if n.islower() and visited:
+                copy_unvisited.remove(n)
+            total += dfs_search2(n, graph, copy_unvisited, visited)
+    return total
+
+nodes = list(graph.keys())
+nodes.remove("start")
+
+part2 = dfs_search2("start", graph, nodes, False)
+print("Part 2: "+str(part2))
