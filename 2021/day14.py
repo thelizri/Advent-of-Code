e3def7344348c5df3b007a6067e325f6e507b701
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-03-21 14:55:53 +0100

    Reading input

--- 2021/day14.py
+++ 2021/day14.py
@@ -4,7 +4,14 @@ file.seek(0)
 li = file.readline().strip()
 print(li)
 
+table = dict()
+
 file.readline()
 for row in file:
-	t = row.strip().split(" -> ")
-	print(t)
+	[pair, insert] = row.strip().split(" -> ")
+	[a, b] = pair
+	key = (a, b)
+	table[key] = insert
+
+
+print(table)
