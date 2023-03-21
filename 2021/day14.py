a0874a5b36efa779b326da5d18698843b7af5fc6
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-03-21 22:32:10 +0100

    Day 14 Done

--- 2021/day14.py
+++ 2021/day14.py
@@ -5,9 +5,15 @@ file = open("day14.txt", "r")
 file.seek(0)
 
 string = file.readline().strip()
-li = [s for s in string]
-table = dict()
+di = dict()
+for i in range(1,len(string)):
+	a = string[i-1]
+	b = string[i]
+	key = (a, b)
+	value = di.get(key, 0)
+	di.update({key: value+1})
 
+table = dict()
 file.readline()
 for row in file:
 	[pair, insert] = row.strip().split(" -> ")
@@ -15,39 +21,54 @@ for row in file:
 	key = (a, b)
 	table[key] = insert
 
+#Methods
+def insertLetterInsidePair(table, di):
+	copy = di.copy()
+	for pair in di.copy().keys():
+		insert = table.get(pair)
+		if insert:
+			v = copy.get(pair)
+			t = di.get(pair)
+			if t-v==0: di.pop(pair)
+			else: di.update({pair: t-v})
+			(a, b) = pair
+			key = (a, insert)
+			value = di.get(key, 0)
+			di.update({key: value+v})
+			key = (insert, b)
+			value = di.get(key, 0)
+			di.update({key: value+v})
+
+def convertDictToListOfValues(di):
+	temp = dict()
+	for pair in di:
+		(a, b) = pair
+		value = di.get(pair, 0)
+		valueInTemp = temp.get(a, 0)
+		temp.update({a: value+valueInTemp})
+		valueInTemp = temp.get(b, 0)
+		temp.update({b: value+valueInTemp})
+	return [round(num/2) for num in list(temp.values())]
+	return list(temp.values())
+
 #Part 1
-def part1(table, li):
-	copy = li.copy()
-	length = len(li)
-	modifier = 0
-	for index in range(1, length):
-		a = li[index-1]
-		b = li[index]
-		key = (a, b)
-		value = table.get(key)
-		if value:
-			copy.insert(index+modifier, value)
-			modifier += 1
-	return copy
-
-def mostAndLeastCommonElement(li):
-	counter = Counter(li)
-	count_list = list(counter.most_common())
-	most = count_list[0][1]
-	least = count_list[-1][1]
-	return most - least
-
 for i in range(10):
-	li = part1(table, li)
+	insertLetterInsidePair(table, di)
 
-result = mostAndLeastCommonElement(li)
-print("Part 1: "+str(result))
+li = convertDictToListOfValues(di)
+li.sort()
+most = li[-1]
+least = li[0]
+answer = most - least + 1
+print(f"Part 1: {answer}")
 
 #Part 2
-# for i in range(30):
-# 	print(i)
-# 	li = part1(table, li)
-# 	print(mostAndLeastCommonElement(li))
+for i in range(30):
+	insertLetterInsidePair(table, di)
 
-# result = mostAndLeastCommonElement(li)
-# print("Part 2: "+str(result))
+li = convertDictToListOfValues(di)
+li.sort()
+most = li[-1]
+least = li[0]
+answer = most - least + 1
+print(f"Part 2: {answer}")
