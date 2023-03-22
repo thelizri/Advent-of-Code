a3e8caf68330576661add5bc1a09bbd7cfd67b62
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-03-22 20:46:54 +0100

    update

--- 2021/day15.py
+++ 2021/day15.py
@@ -9,8 +9,6 @@ for row in file:
 	line = [int(c) for c in row.strip()]
 	matrix.append(line)
 
-print(matrix)
-
 height = len(matrix)
 width = len(matrix[0])
 
@@ -25,23 +23,3 @@ def validCoord(coord):
 def getAdjacent(x, y):
 	candidates = [(x+1,y), (x-1,y), (x,y+1), (x,y-1)]
 	return list(filter(validCoord, candidates))
-
-def dfs_search(x, y, score, visited):
-	if score > 50: return float("inf")
-	score += matrix[y][x]
-	visited.append((x, y))
-
-	if x == width-1 and y == height-1:
-		return score
-
-	candidates = getAdjacent(x, y)
-	neighbors = list(filter(lambda x: x not in visited, candidates))
-	if neighbors:
-		results = []
-		for x,y in neighbors:
-			results.append(dfs_search(x, y, score, visited.copy()))
-		return min(results)
-	else:
-		return float("inf")
-
-print(dfs_search(0, 0, -1, []))
