e5bfa5356f947d94c41e4b38d91c214256f1b0b1
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-03-22 15:25:21 +0100

    M

--- 2021/day15.py
+++ 2021/day15.py
@@ -1,6 +1,5 @@
-import re
-
 #Goal: Find path with lowest total risk
+#Can't move diagonally
 
 #Read input
 file = open("day15.txt", "r")
@@ -12,3 +11,19 @@ for row in file:
 	matrix.append(line)
 
 print(matrix)
+
+height = len(matrix)
+width = len(matrix[0])
+
+def validCoord(coord):
+	(x, y) = coord
+	if x >= 0 and x < width:
+		if y >= 0 and y < height:
+			return True
+	return False
+
+def getAdjacent(x, y):
+	candidates = [(x+1,y), (x-1,y), (x,y+1), (x,y-1)]
+	return list(filter(validCoord, candidates))
+
+print(getAdjacent(1, 0))
