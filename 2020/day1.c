265142c20b234b92cc18804f034d61925a81fe17
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-03-28 23:11:38 +0200

    update

--- 2020/day1.c
+++ 2020/day1.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
-int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
-   return 0;
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    FILE* ptr;
+    char str[50];
+    ptr = fopen("day1.txt", "a+");
+ 
+    if (NULL == ptr) {
+        printf("file can't be opened \n");
+    }
+ 
+    printf("content of this file are \n");
+ 
+    while (fgets(str, 50, ptr) != NULL) {
+        printf("%s", str);
+    }
+ 
+    fclose(ptr);
+    return 0;
 }
