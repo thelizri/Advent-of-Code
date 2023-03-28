5cf9a42295756de3891eddc7f862b4db3c7b3454
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-03-28 23:17:54 +0200

    update

--- 2020/day1.c
+++ 2020/day1.c
@@ -6,17 +6,23 @@ int main()
 {
     FILE* ptr;
     char str[50];
-    ptr = fopen("day1.txt", "a+");
+    ptr = fopen("day1.txt", "r");
  
     if (NULL == ptr) {
         printf("file can't be opened \n");
     }
+
+    int sum = 0;
  
     printf("content of this file are \n");
  
     while (fgets(str, 50, ptr) != NULL) {
-        printf("%s", str);
+        int x = atoi(str);
+        printf("Converting %s: %d\n", str, x);
+        sum += x;
     }
+
+    printf("Sum is: %d\n", sum);
  
     fclose(ptr);
     return 0;
