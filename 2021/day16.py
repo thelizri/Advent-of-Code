05116bf111a28da1b5a122ede872114661142210
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-03-31 23:39:20 +0200

    update

---
+++ 2021/day16.py
@@ -0,0 +1,47 @@
+#Step 1, convert hexadecimal to binary
+# 0 = 0000
+# 1 = 0001
+# 2 = 0010
+# 3 = 0011
+# 4 = 0100
+# 5 = 0101
+# 6 = 0110
+# 7 = 0111
+# 8 = 1000
+# 9 = 1001
+# A = 1010
+# B = 1011
+# C = 1100
+# D = 1101
+# E = 1110
+# F = 1111
+
+#Header
+#the first three bits encode the packet version, and the next three bits encode the packet type ID.
+
+hex_to_bin = {
+    '0': '0000',
+    '1': '0001',
+    '2': '0010',
+    '3': '0011',
+    '4': '0100',
+    '5': '0101',
+    '6': '0110',
+    '7': '0111',
+    '8': '1000',
+    '9': '1001',
+    'A': '1010',
+    'B': '1011',
+    'C': '1100',
+    'D': '1101',
+    'E': '1110',
+    'F': '1111'
+}
+
+def getBinary(string):
+	result = ''
+	for s in string:
+		result += hex_to_bin[s]
+	return result
+
+print(getBinary("D2FE28"))
