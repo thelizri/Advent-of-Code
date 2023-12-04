f9be1b397bd2bae5d98d8b981ae6081f6c66e717
Karlan <wcar@kth.se>
2023-12-04 06:53:00 +0100

    Day 4

--- 2023 /day4/day4.py
+++ 2023 /day4/day4.py
@@ -23,3 +23,22 @@ for row_id, row in enumerate(text, 1):
         part1 += 2 ** (amount_of_winning_numbers - 1)
 
 print("Part 1:", part1)
+
+my_dict = {}
+for i in range(len(text)):
+    my_dict[i + 1] = 1
+
+part2 = 0
+for row_id, row in enumerate(text, 1):
+    _, row = row.split(":")
+    winning_numbers, card_numbers = row.split("|")
+    winning_numbers, card_numbers = set(winning_numbers.split()), set(
+        card_numbers.split()
+    )
+
+    amount_of_winning_numbers = len(winning_numbers.intersection(card_numbers))
+    for i in range(1, amount_of_winning_numbers + 1):
+        my_dict[row_id + i] = my_dict[row_id + i] + my_dict[row_id]
+
+part2 = sum(list(my_dict.values()))
+print("Part 2:", part2)
