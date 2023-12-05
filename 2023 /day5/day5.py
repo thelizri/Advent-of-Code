f13fe03a329c5495030428529689cfc1cb5d45dc
Karlan <wcar@kth.se>
2023-12-05 12:47:57 +0100

    Part 1

---
+++ 2023 /day5/day5.py
@@ -0,0 +1,80 @@
+import re
+
+text = """seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4
+""".split(
+    "\n\n"
+)
+
+text = open("day5.txt").read().split("\n\n")
+
+# dest range start, source range start, length
+
+# Any source numbers that aren't mapped correspond to the same destination number
+
+pattern = re.compile(r"\d+")
+seeds = re.findall(pattern, text[0])
+seed_to_soil = re.findall(pattern, text[1])
+soil_to_fertilizer = re.findall(pattern, text[2])
+fertilizer_to_water = re.findall(pattern, text[3])
+water_to_light = re.findall(pattern, text[4])
+light_to_temperature = re.findall(pattern, text[5])
+temperature_to_humidity = re.findall(pattern, text[6])
+humidity_to_location = re.findall(pattern, text[7])
+
+locations = []
+
+
+def translator(number, map):
+    for i in range(0, len(map), 3):
+        dest_start, source_start, length = int(map[i]), int(map[i + 1]), int(map[i + 2])
+        if number in range(source_start, source_start + length):
+            return dest_start + (number - source_start)
+
+    return number
+
+
+for seed in seeds:
+    soil = translator(int(seed), seed_to_soil)
+    fertilizer = translator(soil, soil_to_fertilizer)
+    water = translator(fertilizer, fertilizer_to_water)
+    light = translator(water, water_to_light)
+    temperature = translator(light, light_to_temperature)
+    humidity = translator(temperature, temperature_to_humidity)
+    location = translator(humidity, humidity_to_location)
+    locations.append(location)
+
+
+part1 = min(locations)
+print("Part 1:", part1)
