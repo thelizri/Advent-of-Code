f1c6663b56313f896590848fae9a774b6b4b3dd4
Karlan <wcar@kth.se>
2023-12-06 13:01:10 +0100

    Part 1, Day 6

--- 2023 /day6/day6.py
+++ 2023 /day6/day6.py
@@ -1,12 +1,35 @@
 import re
+import functools
+import operator
 
 text = """Time:      7  15   30
 Distance:  9  40  200""".splitlines()
 
-# text = open("day6.txt").read().splitlines()
+text = open("day6.txt").read().splitlines()
 
 pattern = re.compile(r"\d+")
 times = [int(number) for number in re.findall(pattern, text[0])]
 distances = [int(number) for number in re.findall(pattern, text[1])]
 
-print(distances)
+# 0 m/s starting speed
+# 1 m/s^2 acceleration
+# the boat can't move until you let go of the button
+# number of ways to beat the record in each race
+
+
+def distance_travelled(time_holding_button, time_of_race):
+    speed = time_holding_button
+    distance = (time_of_race - time_holding_button) * speed
+    return distance
+
+
+part1 = []
+for time, record in zip(times, distances):
+    amount = 0
+    for i in range(time):
+        distance = distance_travelled(i, time)
+        if distance > record:
+            amount += 1
+    part1.append(amount)
+
+print("Part 1:", functools.reduce(operator.mul, part1))
