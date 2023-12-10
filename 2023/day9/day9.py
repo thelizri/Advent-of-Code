41a6232f8d457fc49aee5fcf7e96f93343ae1976
Karlan <wcar@kth.se>
2023-12-10 06:33:30 +0100

    Day 9 Part 1, So tired. So sick

---
+++ 2023/day9/day9.py
@@ -0,0 +1,48 @@
+# Get input
+# Parse input
+# Work down until zeroes
+# Predict next number
+# Do the same for all rows
+# Add up all predicted numbers
+import re
+
+text = """0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45""".splitlines()
+
+text = open("day9.txt").read().splitlines()
+
+pattern = re.compile(r"-?\d+")
+
+
+def sequence_of_differences(sequence):
+    result = []
+    for i, j in zip(sequence[:-1], sequence[1:]):
+        result.append(j - i)
+    return result
+
+
+def is_all_zero(sequence):
+    for number in sequence:
+        if number != 0:
+            return False
+    return True
+
+
+def get_difference(sequence):
+    differences = sequence_of_differences(sequence)
+    if is_all_zero(differences):
+        return 0
+    else:
+        return differences[-1] + get_difference(differences)
+
+
+part1 = 0
+for row in text:
+    # Convert to numbers
+    row_of_numbers = [int(number) for number in re.findall(pattern, row)]
+    difference = get_difference(row_of_numbers)
+    next_number = row_of_numbers[-1] + difference
+    part1 += next_number
+
+print("Part 1:", part1)
