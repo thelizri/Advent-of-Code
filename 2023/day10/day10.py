13511e14e77bbd450dfc18591e34bebe953ac1e7
Karlan <wcar@kth.se>
2023-12-12 19:37:12 +0100

    Part 2, Day 10

--- 2023/day10/day10.py
+++ 2023/day10/day10.py
@@ -1,12 +1,15 @@
 import math
 
-input = """-----
-..F..
-..F7.
-.FJ|.
-SJ.L7
-|F--J
-LJ...""".splitlines()
+input = """FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L""".splitlines()
 
 input = open("day10.txt").read().splitlines()
 
@@ -69,11 +72,63 @@ grid = []
 for r, row in enumerate(input):
     new_row = []
     for c, ch in enumerate(row):
-        if (r, c) in trail or ch == ".":
+        if (r, c) in trail:
             new_row.append(ch)  # Keep the character if it's in the trail or a period
         else:
             new_row.append("#")  # Replace '|' with '#' otherwise
     grid.append("".join(new_row))
 
 
-print("\n".join(grid))
+count = 0
+
+
+def check_left(r, c):
+    count = 0
+    up = False
+    down = False
+    for x in range(c):
+        char = grid[r][x]
+        if char in "|":
+            count += 1
+        elif char in "F7":
+            down = not down
+        elif char in "LJ":
+            up = not up
+        if up and down:
+            count += 1
+            up, down = False, False
+
+    return count % 2 == 1
+
+
+def check_right(r, c, length):
+    count = 0
+    up = False
+    down = False
+    for x in range(c, length):
+        char = grid[r][x]
+        if char in "|":
+            count += 1
+        elif char in "F7":
+            down = not down
+        elif char in "LJ":
+            up = not up
+        if up and down:
+            count += 1
+            up, down = False, False
+
+    return count % 2 == 1
+
+
+for r, row in enumerate(grid):
+    if r == 0 or r == len(grid) - 1:
+        continue
+    for c, char in enumerate(row):
+        if c == 0 or c == len(row) - 1:
+            continue
+        if char != "#":
+            continue
+        if check_left(r, c) and check_right(r, c, len(row)):
+            count += 1
+
+print("Part 2:", count)
