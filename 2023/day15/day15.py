826658a2d588b2607931c822879c0e3220eec578
Karlan <wcar@kth.se>
2023-12-15 19:24:24 +0100

    Part 2, Day 15

--- 2023/day15/day15.py
+++ 2023/day15/day15.py
@@ -18,3 +18,39 @@ for str in input.split(","):
     amount += hash(str)
 
 print("Part 1:", amount)
+
+
+def focusing_power(box_number, contents):
+    total = 0
+    for slot_number, (_, focal_length) in enumerate(contents, 1):
+        total += box_number * slot_number * focal_length
+    return total
+
+
+boxes = {}
+for i in range(256):
+    boxes[i] = []
+
+for instruction in input.split(","):
+    if "-" in instruction:
+        label = instruction[:-1]
+        box_number = hash(label)
+        contents = boxes[box_number]
+        matches = [(l, f) for l, f in contents if label == l]
+        for match in matches:
+            contents.remove(match)
+    else:
+        label, number = instruction.split("=")
+        box_number = hash(label)
+        contents = boxes[box_number]
+        matches = [i for i, (l, _) in enumerate(contents) if label == l]
+        if matches:
+            contents[matches[0]] = (label, int(number))
+        else:
+            contents.append((label, int(number)))
+
+total = 0
+for box_number, contents in enumerate(boxes.values(), 1):
+    total += focusing_power(box_number, contents)
+
+print("Part 2:", total)
