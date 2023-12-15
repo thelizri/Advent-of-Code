66530254814b6158196dd13ea9994c49e2cf79ad
Karlan <wcar@kth.se>
2023-12-15 04:52:20 +0100

    Part 2, Not working

--- 2023/day14/day14.py
+++ 2023/day14/day14.py
@@ -9,7 +9,7 @@ O.#..O.#.#
 #....###..
 #OO..#....""".splitlines()
 
-input = open("day14.txt").read().splitlines()
+# input = open("day14.txt").read().splitlines()
 
 
 def parse_input(input_string):
@@ -33,6 +33,57 @@ def tilt_nort(grid):
     return grid
 
 
+def tilt_south(grid):
+    for col_id in range(len(grid[0])):
+        for row_id in reversed(range(len(grid) - 1)):
+            if grid[row_id][col_id] == "O":
+                # Move rock as far downwards as possible
+                final_destination = row_id
+                while (
+                    final_destination < len(grid) - 1
+                    and grid[final_destination + 1][col_id] == "."
+                ):
+                    final_destination += 1
+                # Switch places between rock and empty space
+                if final_destination != row_id:
+                    grid[final_destination][col_id] = "O"
+                    grid[row_id][col_id] = "."
+    return grid
+
+
+def tilt_west(grid):
+    for row in grid:
+        for col_id in range(1, len(row)):
+            if row[col_id] == "O":
+                # Move rock as far left as possible
+                final_destination = col_id
+                while final_destination > 0 and row[final_destination - 1] == ".":
+                    final_destination -= 1
+                # Switch places between rock and empty space
+                if final_destination != col_id:
+                    row[final_destination] = "O"
+                    row[col_id] = "."
+    return grid
+
+
+def tilt_east(grid):
+    for row in grid:
+        for col_id in reversed(range(len(row) - 1)):
+            if row[col_id] == "O":
+                # Move rock as far right as possible
+                final_destination = col_id
+                while (
+                    final_destination < len(row) - 1
+                    and row[final_destination + 1] == "."
+                ):
+                    final_destination += 1
+                # Switch places between rock and empty space
+                if final_destination != col_id:
+                    row[final_destination] = "O"
+                    row[col_id] = "."
+    return grid
+
+
 def calc_load(grid):
     load = 0
     for r, row in enumerate(reversed(grid), 1):
@@ -40,7 +91,58 @@ def calc_load(grid):
     return load
 
 
+def make_copy(grid):
+    return [row.copy() for row in grid]
+
+
+def compare_grids(grid_a, grid_b):
+    for a, b in zip(grid_a, grid_b):
+        if a != b:
+            return False
+    return True
+
+
+def grid_to_string(grid):
+    return "\n".join("".join(row) for row in grid)
+
+
+def cycle(grid):
+    grid = tilt_nort(grid)
+    grid = tilt_west(grid)
+    grid = tilt_south(grid)
+    grid = tilt_east(grid)
+    return grid
+
+
 grid = parse_input(input)
 grid = tilt_nort(grid)
 load = calc_load(grid)
 print("Part 1:", load)
+
+
+def part2():
+    grid = parse_input(input)
+    states = set(grid_to_string(grid))
+
+    cycle_length = 0
+    while True:
+        grid = cycle(grid)
+        cycle_length += 1
+
+        current_state = grid_to_string(grid)
+        if current_state in states:
+            break
+        else:
+            states.add(current_state)
+
+    print(cycle_length)
+    print("load:", calc_load(grid))
+    remaining_cycles = 1_000_000_000 % cycle_length
+    for _ in range(remaining_cycles + 1):
+        grid = cycle(grid)
+
+    load = calc_load(grid)
+    print("Part 2:", load)
+
+
+part2()
