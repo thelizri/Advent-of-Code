6bbc9115957531df6b7fb780d1e25228fccfd66d
Karlan <wcar@kth.se>
2023-12-24 23:22:03 +0100

    Update

--- 2023/day23/day23.py
+++ 2023/day23/day23.py
@@ -25,13 +25,13 @@ input = """#.#####################
 #####################.#
 """.splitlines()
 
-# input = open("day23.txt").read().splitlines()
+input = open("day23.txt").read().splitlines()
 
 start = (0, 1)
 end = (len(input) - 1, len(input[0]) - 2)
 visited = set()
 
-sys.setrecursionlimit(10000)
+sys.setrecursionlimit(3000)
 
 
 def part1(y, x, steps, visited):
@@ -67,31 +67,29 @@ def part1(y, x, steps, visited):
     return max_steps
 
 
-def part2(y, x, steps, visited):
-    # Check boundaries and walls first
-    if y < 0 or y >= len(input) or x < 0 or x >= len(input[0]) or input[y][x] == "#":
-        return 0
-    # Check if the end is reached
-    if (y, x) == end:
-        return steps
-    # Check if already visited
-    if (y, x) in visited:
-        return 0
-
-    visited.add((y, x))
-
-    max_steps = 0
-
-    for ny, nx in [(y + 1, x), (y - 1, x), (y, x + 1), (y, x - 1)]:
-        if (ny, nx) in visited:
-            continue
-        max_steps = max(max_steps, part2(ny, nx, steps + 1, visited))
-
-    visited.remove((y, x))
-    return max_steps
-
-
 result = part1(start[0], start[1], 0, set())
 print("Part 1:", result)
-result = part2(start[0], start[1], 0, set())
-print("Part 2:", result)
+
+
+# Part 2
+def prune_grid():
+    points = set()
+    for y, row in enumerate(input):
+        for x, c in enumerate(row):
+            if c == "#":
+                continue
+            outgoing = 0
+            for ny, nx in [(y + 1, x), (y - 1, x), (y, x + 1), (y, x - 1)]:
+                if ny < 0 or ny >= len(input) or nx < 0 or nx >= len(input[0]):
+                    continue
+                if input[ny][nx] == "#":
+                    continue
+                outgoing += 1
+            if outgoing > 2:
+                points.add((y, x))
+    return points
+
+
+points = prune_grid()
+print(len(points))
+print(points)
