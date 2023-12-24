3ebf91e5258d98af79e9dd74eb0403570834baf4
Karlan <wcar@kth.se>
2023-12-24 18:34:49 +0100

    Part 1, Refactor

--- 2023/day21/day21.py
+++ 2023/day21/day21.py
@@ -13,6 +13,7 @@ input = """...........
 ...........""".splitlines()
 
 input = open("day21.txt").read().splitlines()
+height, width = len(input), len(input[0])
 
 # (S), garden plots (.), and rocks (#)
 queue = deque()
@@ -25,31 +26,38 @@ def find_start(garden):
                 return y, x
 
 
-def flood_fill(garden, queue):
+def flood_fill(garden, queue, visited):
     result = deque()
     while queue:
         y, x = queue.popleft()
-        if y < 0 or y >= len(garden) or x < 0 or x >= len(garden[y]):
+        if y < 0 or y >= height or x < 0 or x >= width:
             continue
         if garden[y][x] == "#":
             continue
         for y, x in [(y + 1, x), (y - 1, x), (y, x + 1), (y, x - 1)]:
-            if y < 0 or y >= len(garden) or x < 0 or x >= len(garden[y]):
+            if y < 0 or y >= height or x < 0 or x >= width:
                 continue
             if garden[y][x] == "#":
                 continue
-            if (y, x) in result:
+            if (y, x) in visited:
                 continue
             result.append((y, x))
+            visited.add((y, x))
     return result
 
 
 def part1():
     start, queue = find_start(input), deque()
     queue.append(start)
-    for i in range(64):
-        queue = flood_fill(input, queue)
-    result = len(set(queue))
+    visited_odd = set()
+    visited_even = set()
+    for i in range(1, 65):
+        if i % 2 == 0:
+            queue = flood_fill(input, queue, visited_even)
+        else:
+            queue = flood_fill(input, queue, visited_odd)
+
+    result = len(visited_even)
     print(result)
 
 
