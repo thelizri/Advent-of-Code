047b32779c5498d3f938fae41c82fa8a97bcbaf5
Karlan <wcar@kth.se>
2023-12-25 16:59:16 +0100

    Update

--- 2023/day22/day22.py
+++ 2023/day22/day22.py
@@ -1,53 +1,44 @@
 from collections import deque
+import re
 
 input = """1,0,1~1,2,1
 0,0,2~2,0,2
-0,2,3~2,2,3
-0,0,4~0,2,4
-2,0,5~2,2,5
-0,1,6~2,1,6
-1,1,8~1,1,9""".splitlines()
+0,2,3~2,2,3""".splitlines()
 
 
 def parse_input(input):
+    pattern = r"\d+"
     blocks = []
     for row in input:
-        left, right = row.split("~")
-        left = [int(x) for x in left.split(",")]
-        right = [int(x) for x in right.split(",")]
-        blocks.append((left, right))
+        matches = re.findall(pattern, row)
+        block = [int(x) for x in matches]
+        block[3] += 1
+        block[4] += 1
+        blocks.append(block)
 
     return blocks
 
 
-def intersection_rectangles(rect1, rect2):
-    start1, end1 = rect1
-    start2, end2 = rect2
-    x1, y1, _ = start1
-    xend1, yend1, _ = end1
-    x2, y2, _ = start2
-    xend2, yend2, _ = end2
-
+def intersection(rect1, rect2):
+    x1, y1, _, xe1, ye1, _ = rect1
+    x2, y2, _, xe2, ye2, _ = rect2
     x = max(x1, x2)
-    xend = min(xend1, xend2)
-    if xend <= x:
+    xend = min(xe1, xe2)
+    if x >= xend:
         return False
 
     y = max(y1, y2)
-    yend = min(yend1, yend2)
-    if yend <= y:
+    yend = min(ye1, ye2)
+    if y >= yend:
         return False
 
     return True
 
 
 def sort_blocks(blocks):
-    blocks.sort(key=lambda x: x[0][2])
+    blocks.sort(key=lambda x: x[2])
     return blocks
 
 
 blocks = parse_input(input)
 blocks = sort_blocks(blocks)
-print(blocks)
-# Make rectangles fall
-# Find which can be vaproized
