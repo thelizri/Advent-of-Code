e15faf13cb91bfb7c653400b39fec549dc4e0ac6
Karlan <wcar@kth.se>
2023-12-25 17:21:05 +0100

    Example Part 1 Works

--- 2023/day22/day22.py
+++ 2023/day22/day22.py
@@ -3,7 +3,11 @@ import re
 
 input = """1,0,1~1,2,1
 0,0,2~2,0,2
-0,2,3~2,2,3""".splitlines()
+0,2,3~2,2,3
+0,0,4~0,2,4
+2,0,5~2,2,5
+0,1,6~2,1,6
+1,1,8~1,1,9""".splitlines()
 
 
 def parse_input(input):
@@ -40,10 +44,6 @@ def sort_blocks(blocks):
     return blocks
 
 
-blocks = parse_input(input)
-blocks = sort_blocks(blocks)
-
-
 def falling(blocks):
     for i, block in enumerate(blocks):
         minz, z = 1, block[2]
@@ -57,5 +57,38 @@ def falling(blocks):
             block[5] -= diff
 
 
+def part1(blocks):
+    supported = {i: [] for i in range(len(blocks))}
+    support = {i: [] for i in range(len(blocks))}
+    for i, block in enumerate(blocks):
+        z1 = block[5]
+        for j, block2 in enumerate(blocks[i + 1 :], i + 1):
+            z2 = block2[2]
+            if z2 <= z1:
+                continue
+            if z2 > z1 + 1:
+                break
+            if intersection(block, block2):
+                supported[j].append(i)
+                support[i].append(j)
+
+    count = 0
+    for value in support.values():
+        if len(value) == 0:
+            count += 1
+        else:
+            has_support = True
+            for j in value:
+                if len(supported[j]) <= 1:
+                    has_support = False
+                    break
+            if has_support:
+                count += 1
+
+    print("Part 1:", count)
+
+
+blocks = parse_input(input)
+sort_blocks(blocks)
 falling(blocks)
-print(blocks)
+part1(blocks)
