73e4e7f022a7c6d4b0d4e5245895839ebfb3d9e9
Karlan <wcar@kth.se>
2023-12-26 17:49:50 +0100

    Not working

--- 2023/day24/day24.py
+++ 2023/day24/day24.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+# px py pz @ vx vy vz
 example_input = """19, 13, 30 @ -2,  1, -2
 18, 19, 22 @ -1, -1, -2
 20, 25, 34 @ -2, -2, -4
@@ -7,5 +10,17 @@ example_input = """19, 13, 30 @ -2,  1, -2
 vectors = []
 for row in example_input:
     row = row.replace(" @", ",")
-    numbers = [int(x) for x in row.split(",")]
-    vectors.append(numbers)
+    px, py, pz, vx, vy, vz = [int(x) for x in row.split(",")]
+    A = py
+    B = -px
+    C = py * vx - px * vy
+    vectors.append((A, B, C))
+
+print(vectors)
+
+for i, a in enumerate(vectors):
+    for j, b in enumerate(vectors[i + 1 :], i + 1):
+        first = np.array([[a[0], a[1]], [b[0], b[1]]])
+        second = np.array([a[2], b[2]])
+        x, y = np.linalg.solve(first, second)
+        print("x: {:.1f}, y: {:.1f}".format(x, y))
