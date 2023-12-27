50663d8705ba4f78730b2a782deb2c3d58806c6b
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-12-27 01:49:11 +0100

    Refactor

--- 2023/day20/day20.py
+++ 2023/day20/day20.py
@@ -1,6 +1,12 @@
 from collections import deque
 
-input = r"""broadcaster -> a
+example_input = r"""broadcaster -> a, b, c
+%a -> b
+%b -> c
+%c -> inv
+&inv -> a""".splitlines()
+
+example_input2 = r"""broadcaster -> a
 %a -> inv, con
 &inv -> b
 %b -> con
@@ -8,107 +14,101 @@ input = r"""broadcaster -> a
 
 input = open("day20.txt").read().splitlines()
 
-# % = flip flop, & = nand, # broadcaster sends input to all receivers
+# (to, signal, from)
 signal_queue = deque()
-modules = {}
 
-# module = {"type": type, "receiver": [], "value": value}, default value is 0
-high, low = 0, 0
-modules["output"] = {"type": "output", "receiver": [], "value": 0}
+
+class Module:
+    def __init__(self, name, type, value=0, outputs=[], inputs=None):
+        self.name = name
+        if type == "%":
+            self.type = "FLIPFLOP"
+        elif type == "&":
+            self.type = "NAND"
+        elif type == "broadcaster":
+            self.type = "BROADCAST"
+        else:
+            self.type = "OUTPUT"
+        self.outputs = outputs
+        self.value = value
+
+        if inputs is None:
+            self.inputs = {}
+        else:
+            self.inputs = inputs
+
+    def receive_signal(self, signal, source):
+        if self.type == "FLIPFLOP":
+            self.flip_flop(signal)
+        elif self.type == "NAND":
+            self.nand(signal, source)
+        elif self.type == "BROADCAST":
+            self.broadcast(signal)
+
+    def send_signal(self, signal):
+        for destination in self.outputs:
+            signal_queue.append((destination, signal, self.name))
+
+    def flip_flop(self, signal):
+        if signal == 0:  # Only Flips on low signal
+            self.value = 1 if self.value == 0 else 0
+            self.send_signal(self.value)
+
+    def nand(self, signal, source):
+        self.inputs[source] = signal
+        if all(self.inputs.values()):
+            self.value = 0
+        else:
+            self.value = 1
+        self.send_signal(self.value)
+
+    def broadcast(self, signal):
+        self.value = signal
+        self.send_signal(self.value)
+
+    def __str__(self):
+        return f"{self.name} {self.type} {self.value} {self.inputs} {self.outputs}"
 
 
 def parse_input(input):
+    modules = {}
     for row in input:
-        left, right = row.split(" -> ")
-        receiver = right.split(", ")
-        inputs = None
-        if left[0] == "%":
-            type = "%"
-            value = 0
-            name = left[1:]
-        elif left[0] == "&":
-            type = "&"
-            value = 1
-            name = left[1:]
-            inputs = {}
-            for row in input:
-                left, right = row.split(" -> ")
-                if name in right:
-                    left = left if left == "broadaster" else left[1:]
-                    inputs[left] = 0
+        broadcaster, receivers = row.split(" -> ")
+        receivers = [r.strip() for r in receivers.split(",")]
+        if broadcaster[0] in "%&":
+            type = broadcaster[0]
+            broadcaster = broadcaster[1:]
+            modules[broadcaster] = Module(broadcaster, type, outputs=receivers)
         else:
-            type = "broadcaster"
-            name = left
-            value = 0
-        if inputs:
-            modules[name] = {
-                "type": type,
-                "receiver": receiver,
-                "value": value,
-                "name": name,
-                "inputs": inputs,
-            }
-        else:
-            modules[name] = {
-                "type": type,
-                "receiver": receiver,
-                "value": value,
-                "name": name,
-            }
+            modules[broadcaster] = Module(broadcaster, broadcaster, outputs=receivers)
+
+    for module in modules.values():
+        if module.type == "NAND":
+            for possible_input_module in modules.values():
+                if possible_input_module.name == module.name:
+                    continue
+                if module.name in possible_input_module.outputs:
+                    module.inputs[possible_input_module.name] = 0
+    return modules
 
 
-def flip_value(value):
-    return 1 if value == 0 else 0
+def part1(input):
+    modules = parse_input(input)
+    low, high = 0, 0
+    # Press button
+    for i in range(1000):
+        signal_queue.append(("broadcaster", 0, "button"))
+        while signal_queue:
+            destination, signal, source = signal_queue.popleft()
+            if signal == 1:
+                high += 1
+            else:
+                low += 1
+            if destination in modules:
+                modules[destination].receive_signal(signal, source)
+
+    answer = high * low
+    print("Part 1:", answer)
 
 
-def increment_counter(signal):
-    global high, low
-    if signal == 1:
-        high += 1
-    else:
-        low += 1
-
-
-def interpret_signal(signal, wire):
-    if wire == "output":
-        return None
-
-    module = modules[wire]
-    type = module["type"]
-    if type == "%":
-        # Flips value if signal is 0
-        if signal == 0:
-            module["value"] = flip_value(module["value"])
-        else:
-            return None
-    elif type == "&":
-        values = module["inputs"].values()
-        if all(values):
-            module["value"] = 0
-        else:
-            module["value"] = 1
-    elif type == "broadcaster":
-        module["value"] = signal
-
-    # Send signal to receivers
-    for receiver in module["receiver"]:
-        increment_counter(module["value"])
-        if receiver in modules:
-            signal_queue.append((module["value"], receiver))
-            if modules[receiver]["type"] == "&":
-                modules[receiver]["inputs"][wire] = module["value"]
-
-
-def press_button():
-    increment_counter(0)
-    signal_queue.append((0, "broadcaster"))
-    while signal_queue:
-        signal, wire = signal_queue.popleft()
-        interpret_signal(signal, wire)
-
-
-parse_input(input)
-for i in range(1000):
-    press_button()
-result = high * low
-print("Result: {}".format(result))
+part1(input)
